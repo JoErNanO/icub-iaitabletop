3d6ba47536afa46f32a905d8177e1ceb7698aa3c
Francesco Giovannini <francesco.giovannini@inria.fr>
2014-07-18 16:53:59 +0200

    [MODULE] [DOC] Adding serial port configuration to documentation.

--- src/modules/IAITableTopController/include/IAITableTopControllerModule.h
+++ src/modules/IAITableTopController/include/IAITableTopControllerModule.h
@@ -1,8 +1,8 @@
-/* 
+/*
  * Copyright (C) 2014 Francesco Giovannini, iCub Facility - Istituto Italiano di Tecnologia
  * Authors: Francesco Giovannini
  * email:   francesco.giovannini@iit.it
- * website: www.robotcub.org 
+ * website: www.robotcub.org
  * Permission is granted to copy, distribute, and/or modify this program
  * under the terms of the GNU General Public License, version 2 or any
  * later version published by the Free Software Foundation.
@@ -21,9 +21,9 @@
 /**
  * @ingroup icub_module
  * \defgroup IAITableTopController IAITableTopController
- * The IAITableTopController is a module which controls the position of the IAI TableTop robot. 
+ * The IAITableTopController is a module which controls the position of the IAI TableTop robot.
  * This module also cyclically polls the robot position and axis status and this information is output on two yarp ports.
- *  
+ *
  *
  * \section intro_sec Description
  * The IAI TableTop robot (http://www.intelligentactuator.com/tt-table-top-robot/) is a 3-axis robot which is controlled by sending position commands via a USB serial port.
@@ -34,7 +34,7 @@
  * From these coordinates the module builds the commands to be sent to the robot.
  * The robot uses a control loop to check if the movement is completed before any new movement command can be sent.
  *
- * 
+ *
  * \section lib_sec Libraries and Dependencies
  * This module depends on various YARP libraries.
  *
@@ -45,10 +45,10 @@
  *      ENABLE_yarpmod_serialport       ON
  *
  * when building YARP from sources.
- * 
- * 
+ *
+ *
  * \section parameters_sec Parameters
- * <b>Command-line Parameters</b> 
+ * <b>Command-line Parameters</b>
  * - -- from : Module ini configuration file.
  *
  *
@@ -57,20 +57,50 @@
  * - period : The module period in seconds.
  * - serialPortConfFile : The serial port configuration filename.
  * - experimentConfFile : The experiment configuration filename.
- * 
- * 
+ *
+ *
  * \section outputports_sec Output Ports
  * - /IAITableTopController/posreader/position:o [yarp::sig::Vector]  [default carrier:tcp]: This port outputs the (X, Y, Z) coordinates of the robot position.
  * - /IAITableTopController/posreader/status:o [yarp::sig::Vector]  [default carrier:tcp]: This port outputs the (X, Y ,Z) axis status flags.
  * - /IAITableTopController/controller/experiment/status:o [yarp::sig::Vector] [default carrier:tcp]: This port outputs the status (step n., time start, time end) of each experiment step. This data is useful when performing data analysis on the recorded signals later on.
- * 
- * 
+ *
+ *
  * \section conf_file_sec Configuration Files
  * - serialPortConfFile : The serial port configuration file.
  * - experimentConfFile : The experiment configuration file.
- * 
- * <b>Experiment Configuration File</b>
  *
+ * <b>Serial Port Configuration File</b><br />
+ * The skeleton of this file was taken from the YARP serialport module here: https://github.com/robotology/yarp/blob/master/src/modules/serial/serial.ini
+ * The parameters depend on the serial device connected to the host machine and must be configured accordingly.
+ * Below is an extract from the configuration file specific to the IAI Tabletop robot:
+ *
+ *     verbose 0
+ *     # The port name
+ *     comport /dev/ttyUSB0
+ *     #comport COM3
+ *     # The baud rate
+ *     baudrate 115200
+ *     # The parity mode (EVEN, ODD, NONE)
+ *     paritymode NONE
+ *     # The number of data bits
+ *     databits 8
+ *     # The number of stop bits
+ *     stopbits 1
+ *     ...
+ *
+ * <b>Serial Port Name - comport</b><br />
+ * In particular, the <i>comport</i> parameter must be changed to reflect the name of the serial port mounted on the host machine.
+ * A typical Linux serial port configuration will look like this:
+ *
+ *     # The port name - Linux
+ *     comport /dev/ttyUSB0
+ *
+ * Whereas a typical Windows serial port configuration will look like this:
+ *
+ *     # The port name - Windows
+ *     comport COM3
+ *
+ * <b>Experiment Configuration File</b><br />
  * The module expects a configuration file for the experiment to be run using the robot.
  * This file is structured as follows:
  *
@@ -85,23 +115,23 @@
  *     posVelAccDecTime (
  *          coordinate_X ...Y ...Z velocity_X ...Y ...Z acceleration_X ...Y ...Z deceleration_X ...Y ...Z step_time \
  *          coordinate_X ...Y ...Z velocity_X ...Y ...Z acceleration_X ...Y ...Z deceleration_X ...Y ...Z step_time \
- *          ... 
+ *          ...
  *     )
- * 
+ *
  * \section tested_os_sec Tested OS
  * Linux, Windows
- * 
+ *
  *
  * \section example_sec Example Instantiation of the Module
  * IAITableTopController --from confIAITableTopController.ini
- * 
- * 
+ *
+ *
  * \author Francesco Giovannini (francesco.giovannini@iit.it)
- * 
+ *
  * Copyright (C) 2014 Francesco Giovannini, iCub Facility - Istituto Italiano di Tecnologia
- * 
+ *
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
- * 
+ *
  * This file can be edited at src/modules/IAITableTopController/include/IAITableTopControllerModule.h
  */
 
@@ -147,7 +177,7 @@ namespace IAITableTop {
             yarp::dev::PolyDriver clientSerial;
             /** The Serial port interface. */
             yarp::dev::ISerialDevice *iSerialPort;
-     
+
             /* ******* Experiment attributes                        ******* */
             /** The experiment step counter. */
             int stepCounter;
@@ -162,7 +192,7 @@ namespace IAITableTop {
             /** The (X, Y, Z) decelerations to be used for moving during the experiment. */
             ParamMatrixXYZ decelerations;
             /** The time intervals during which each position is to be maintained. */
-            std::vector<int> timeIntervals;        
+            std::vector<int> timeIntervals;
 
             /* ****** Ports                                         ****** */
             /** The port timestamp. */
@@ -174,7 +204,7 @@ namespace IAITableTop {
             /** Output port for experiment step information. */
             yarp::os::BufferedPort<yarp::sig::Vector> portIAITTControllerOutExperimentStatus;
 
-     
+
             /* ****** Debug attributes                              ****** */
             std::string dbgTag;
 
@@ -210,7 +240,7 @@ namespace IAITableTop {
              * \param o_cmd The generated command string
              */
             void buildMoveAbsX(const int &i_pos, yarp::os::Bottle &o_cmd );
-            
+
             /**
              * Build the movement command for the Y axis.
              *
@@ -218,7 +248,7 @@ namespace IAITableTop {
              * \param o_cmd The generated command string
              */
             void buildMoveAbsY(const int &i_pos, yarp::os::Bottle &o_cmd );
-            
+
             /**
              * Build the movement command for the Z axis.
              *
@@ -248,7 +278,7 @@ namespace IAITableTop {
              * \return True if the the command is sent and exectured correctly
              */
             bool sendCommandToSerial(const yarp::os::Bottle &i_cmd, std::string &o_data);
-            
+
             /**
              * Read the data from the serial and check if the robot returned an error.
              *
